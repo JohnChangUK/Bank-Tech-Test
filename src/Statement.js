0972836b1d4848ec092b5e4e3bfc089bf5ec9846
John Chang <johnchang9094@gmail.com>
2017-04-18 17:27:35 +0100

    Implemented Headers for Statement

--- src/Statement.js
+++ src/Statement.js
@@ -2,3 +2,14 @@ function Statement(account) {
     this.account = account;
 };
 
+Statement.prototype.displayAccount = function() {
+    return this.account;
+};
+
+Statement.prototype.displayHeaders = function() {
+    return("   date    ||  credit  ||  debit  || balance \n");
+};
+
+Statement.prototype.displayTransactions = function() {
+    
+}
